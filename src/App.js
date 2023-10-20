e0b02088d5578e1d20ea72131f6f4987dc57be12
harpreetbhangu26 <heronoone26@gmail.com>
2023-10-20 21:59:17 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/' element={<LoginPage/>} />
       <Route path='/home' element={<Home/>}/>
         
       </Routes>
